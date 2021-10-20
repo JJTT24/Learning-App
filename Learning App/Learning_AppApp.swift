325ddec29f8e5627792d880d83b443dd44a7e6d5
Jontrell Foster <fjontrell@gmail.com>
2021-10-20 15:36:58 -0400

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Jontrell Foster II on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
